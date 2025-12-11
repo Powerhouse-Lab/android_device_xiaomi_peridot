430f3f803f138781bce0fb02dd020d713d0b9ef6
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 12:21:55 +0200

    peridot: Patch qccsyshal to load versioned libprotobuf
    Change-Id: I9ec95bf55afd62a03b2bd787c8b82c15b921a452

--- extract-files.py
+++ extract-files.py
@@ -61,6 +61,11 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
         .remove_needed('android.hidl.base@1.0.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed(
+            'libprotobuf-cpp-full.so',
+            'libprotobuf-cpp-full-21.7.so'
+    ),
     (
         'odm/etc/camera/enhance_motiontuning.xml',
         'odm/etc/camera/motiontuning.xml',
