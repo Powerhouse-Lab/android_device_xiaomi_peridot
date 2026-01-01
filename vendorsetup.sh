6105cf1dec0c446fc858332673726e3327d8626e
zenin1504 <kayozzprojects@yandex.com>
2026-01-02 06:05:04 +0800

    peridot: Add dolby patches on source
    Change-Id: I9e8297c35c9042e7866a5151ccecbdd0
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,37 @@
+cd hardware/qcom-caf/sm8650/audio/agm
+git fetch https://github.com/sm8635-dev/vendor_qcom_opensource_agm lineage-23.2-caf-sm8650
+git reset --hard FETCH_HEAD
+croot
+
+cd hardware/qcom-caf/sm8650/audio/graphservices
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices lineage-23.2-caf-sm8650
+git reset --hard FETCH_HEAD
+croot
+
+cd hardware/qcom-caf/sm8650/audio/pal
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx lineage-23.2-caf-sm8650
+git reset --hard FETCH_HEAD
+croot
+
+cd hardware/qcom-caf/sm8650/audio/primary-hal
+git fetch https://github.com/LineageOS/android_hardware_qcom_audio-ar lineage-23.2-caf-sm8650
+git reset --hard FETCH_HEAD
+croot
+
+cd hardware/lineage/interfaces
+git fetch https://github.com/sm8635-dev/hardware_lineage_interfaces sixteen
+git reset --hard FETCH_HEAD
+croot
+
+cd device/lineage/sepolicy
+git fetch https://github.com/sm8635-dev/device_lineage_sepolicy sixteen
+git reset --hard FETCH_HEAD
+croot
+
+cd frameworks/av
+git fetch https://github.com/sm8635-dev/frameworks_av
+git cherry-pick b17a39b6e6359f8cc5174de64dde58115459688d 09e19c1a215b1bd714e8dff4ca7f04c9e3eb6a50
+croot
+
+rm -rf vendor/yaap/signing/keys
+git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/signing/keys
