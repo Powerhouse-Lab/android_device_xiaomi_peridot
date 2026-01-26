64e4336651ef1919c4bf1f89f3a3aed98bd8d4db
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-26 20:58:36 +0530

    peridot: Remove parts app sepolicy
    Change-Id: Ic88d29be3fe8e5dc563251a8f5157a77387e0625

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,7 +226,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor security patch
 VENDOR_SECURITY_PATCH := 2025-08-01
