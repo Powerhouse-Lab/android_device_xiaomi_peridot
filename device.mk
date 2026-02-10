afeeb8975a5a6cb325e596f79ec0c8433a4e5aca
Powerhouse <Rautnikhil373@gmail.com>
2026-02-10 06:47:35 +0000

    Revert "peridot: Switch to AOSP strongbox keymint and weaver"
    This reverts commit 3997b48da377712e1d8f9a9d6de4bbf4dfe11c23.

--- device.mk
+++ device.mk
@@ -230,8 +230,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymint
 PRODUCT_PACKAGES += \
-    android.hardware.security.keymint3-service.strongbox.nxp \
-    android.hardware.weaver-service.nxp
+    android.hardware.authsecret-service.nxp
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
