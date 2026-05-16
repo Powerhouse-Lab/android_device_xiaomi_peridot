bc5d633a590dcc7227c016fdb27432b19eb0bea6
Shikhar <krtonia@gmail.com>
2026-05-16 23:59:54 +0530

    peridot: include vendorsetup
    Signed-off-by: Shikhar <krtonia@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Define colour codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+NC="\033[0m" # No Color
+
+fatal() {
+    echo -e "${RED}[FATAL] $1${NC}"
+    return 1
+}
+
+info() {
+    echo -e "${CYAN}$1${NC}"
+}
+
+success() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+warn() {
+    echo -e "${YELLOW}$1${NC}"
+}
+
+info "Cloning All resources"
+
+# Vendor
+info "Cloning vendor tree"
+git clone -b 16.2 --depth 1 https://github.com/Poco-F6-resources/android_vendor_xiaomi_peridot.git vendor/xiaomi/peridot || fatal "Vendor tree clone failed!"
+
+# Kernel sources
+info "Cloning Kernel sources"
+git clone -b 16.2 --depth 1 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635 || fatal "Kernel source clone failed!"
+
+warn "Cleaning kernel modules directory (if exists)"
+rm -rf kernel/xiaomi/sm8635-modules
+info "Cloning kernel modules"
+git clone -b 16.2 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules || fatal "Kernel modules clone failed!"
+
+warn "Cleaning kernel devicetrees directory (if exists)"
+rm -rf kernel/xiaomi/sm8635-devicetrees
+info "Cloning kernel devicetrees"
+git clone -b 16.2 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees || fatal "Kernel devicetrees clone failed!"
+
+# Hardware xiaomi
+info "Cloning hardware xiaomi"
+warn "Cleaning hardware/xiaomi directory (if exists)"
+rm -rf hardware/xiaomi
+git clone -b 16.2 https://github.com/Poco-F6-resources/hardware_xiaomi.git hardware/xiaomi || fatal "Hardware xiaomi clone failed!"
+
+# Dolby
+info "Cloning Lunaris Dolby"
+warn "Cleaning Old Dolby repo"
+git clone -b 16 https://github.com/Poco-F6-resources/hardware_dolby.git hardware/dolby || fatal "Dolby clone failed"
+
+# GameBar
+info "Cloning GameBar from Kenway"
+warn "Cleaning up old packages/apps/GameBar directory"
+rm -rf packages/apps/GameBar
+git clone -b lineage-23.2 https://github.com/peridot-hyperos-2/packages_apps_GameBar.git packages/apps/GameBar || fatal "GameBar clone failed!"
+
+# BCR
+info "Cloning BCR"
+warn "Cleaning BCR"
+rm -rf vendor/bcr
+git clone -b main https://github.com/Poco-F6-resources/vendor_bcr.git vendor/bcr || fatal "BCR clone failed!"
+
+# Mi Cam
+info "Cloning Mi Cam"
+info "Cloning Miuicamera vendor"
+git clone -b 16.2 --depth 1 https://github.com/Poco-F6-resources/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera || fatal "Vendor miuicamera clone failed!"
+
+info "Cloning Miuicamera device"
+git clone -b 16.2 --depth 1 https://github.com/Poco-F6-resources/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera || fatal "Device miuicamera clone failed!"
+
+# Keys
+info "Cloning your keys inf-keys"
+git clone -b 16 git@github.com:ProjectInfinity-X/vendor_infinity-priv_keys.git vendor/infinity-priv/keys/testkey
+success "All resources cloned successfully!"
+
+return 0
