9fefdf9cebf6cd159d22b425868a76a6074af2dc
Powerhouse <rautnikhil373@gmail.com>
2026-08-15 12:35:43 +0000

    peridot: Use legacy libion implementation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,6 +217,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
+include device/voltage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
