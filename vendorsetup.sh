ea074e120a38548393cec7b326ef1e33af6a2829
Shikhar <krtonia@gmail.com>
2026-08-30 18:28:06 +0530

    peridot: clone 17 branch for micam
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,6 +62,6 @@ info "Cloning Miuicamera vendor from Grewal"
 git clone -b sixteen-qpr2 --depth 1 https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera || fatal "Vendor miuicamera clone failed!"
 
 info "Cloning Miuicamera device"
-git clone -b lineage-23.2 --depth 1 https://github.com/Poco-F6-resources/android_device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera || fatal "Device miuicamera clone failed!"
+git clone -b 17 --depth 1 https://github.com/Poco-F6-resources/android_device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera || fatal "Device miuicamera clone failed!"
 
 return 0
